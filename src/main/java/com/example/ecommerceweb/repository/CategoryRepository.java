c2dfcd0da97ec59909e6f5f2115eec7c65459c01
coinhat <nhatcoifacebook@gmail.com>
2024-10-15 16:15:59 +0700

    feat: add CategoryRepository

--- src/main/java/com/example/ecommerceweb/repository/CategoryRepository.java
+++ src/main/java/com/example/ecommerceweb/repository/CategoryRepository.java
@@ -2,7 +2,8 @@ package com.example.ecommerceweb.repository;
 
 import com.example.ecommerceweb.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-
 }
