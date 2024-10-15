2d72d47e62f147b6b15a6d5ae790e9ed7b6bbc2b
coinhat <nhatcoifacebook@gmail.com>
2024-10-15 15:43:54 +0700

    feat: add product and base entity

---
+++ src/main/java/com/example/ecommerceweb/entities/Product.java
@@ -0,0 +1,37 @@
+package com.example.ecommerceweb.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "products")
+@Entity
+@Builder
+public class Product extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 255)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private Float price;
+
+    @Column(name = "thumbnail", length = 255)
+    private String thumbnail;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category categoryId;
+}
