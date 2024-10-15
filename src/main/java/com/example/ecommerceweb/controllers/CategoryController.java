73af0d3e3a52485593765c165d3766ac9f837c9a
coinhat <nhatcoifacebook@gmail.com>
2024-10-15 15:58:04 +0700

    feat: add test category controller

--- src/main/java/com/example/ecommerceweb/controllers/CategoryController.java
+++ src/main/java/com/example/ecommerceweb/controllers/CategoryController.java
@@ -1,6 +1,8 @@
 package com.example.ecommerceweb.controllers;
 
+import com.example.ecommerceweb.dtos.CategoryDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,8 +12,10 @@ public class CategoryController {
 
 
     @PostMapping("")
-    public String createCategory() {
+    public String createCategory(@ModelAttribute CategoryDTO categoryDTO) {
         return "redirect:/";
     }
+
+
 
 }
