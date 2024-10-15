c08ce0d598d667a3a3a074c24ad94d96ee01fc95
coinhat <nhatcoifacebook@gmail.com>
2024-10-15 15:42:26 +0700

    feat: add Category entity

---
+++ src/main/java/com/example/ecommerceweb/entities/Category.java
@@ -0,0 +1,22 @@
+package com.example.ecommerceweb.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "categories")
+@Entity
+@Builder
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
