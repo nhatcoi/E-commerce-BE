fb9dd15929fb714485f5b86163e05a74acf993bc
coinhat <nhatcoifacebook@gmail.com>
2024-10-15 15:57:42 +0700

    feat: add category dto

---
+++ src/main/java/com/example/ecommerceweb/dtos/CategoryDTO.java
@@ -0,0 +1,14 @@
+package com.example.ecommerceweb.dtos;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CategoryDTO {
+    private Long id;
+    private String name;
+}
+
