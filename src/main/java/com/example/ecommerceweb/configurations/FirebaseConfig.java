7292d94260c7162ba68f0a35af3676188814dba2
coinhat <nhatcoifacebook@gmail.com>
2024-10-17 00:27:22 +0700

    feat: add FirebaseConfig

---
+++ src/main/java/com/example/ecommerceweb/configurations/FirebaseConfig.java
@@ -0,0 +1,25 @@
+package com.example.ecommerceweb.configurations;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Bean
+    public FirebaseApp initializeFirebaseApp() throws IOException {
+        String serviceAccountPath = System.getProperty("user.dir") + "/ecommerce-web-key.json";
+        FileInputStream serviceAccountStream = new FileInputStream(serviceAccountPath);
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccountStream))
+                .setStorageBucket("ecommerce-web-18f77.appspot.com")
+                .build();
+        return FirebaseApp.initializeApp(options);
+    }
+}
