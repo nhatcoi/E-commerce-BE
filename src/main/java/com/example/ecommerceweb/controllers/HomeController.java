47c0edc43aa0e60f393043a16ab2418f3a286d03
coinhat <nhatcoifacebook@gmail.com>
2024-10-23 18:20:08 +0700

    feat: latest product

---
+++ src/main/java/com/example/ecommerceweb/controllers/HomeController.java
@@ -0,0 +1,50 @@
+package com.example.ecommerceweb.controllers;
+
+import com.example.ecommerceweb.dtos.ProductDTO;
+import com.example.ecommerceweb.entities.Category;
+import com.example.ecommerceweb.entities.Product;
+import com.example.ecommerceweb.services.CategoryService;
+import com.example.ecommerceweb.services.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("")
+@RequiredArgsConstructor
+public class HomeController {
+    private final CategoryService categoryService;
+    private final ProductService productService;
+
+    @GetMapping("")
+    public String home(Model model) throws IOException, InterruptedException {
+        List<Category> categories = categoryService.getAllCategories();
+        List<Product> products = productService.getAllProducts();
+
+        List<Product> sortedProducts = products.stream()
+                .sorted((p1, p2) -> p2.getCreatedAt().compareTo(p1.getCreatedAt()))
+                .toList();
+
+        List<Product> latestProductsLeft = sortedProducts.stream().limit(3).toList();
+        List<Product> latestProductsRight = sortedProducts.stream().skip(3).limit(3).toList();
+
+        model.addAttribute("categories", categories);
+        model.addAttribute("products", products);
+        model.addAttribute("latestProductsLeft", latestProductsLeft);
+        model.addAttribute("latestProductsRight", latestProductsRight);
+
+
+        return "index";
+    }
+
+
+
+
+
+}
