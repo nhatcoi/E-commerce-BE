d0a46dee23b24c2b30e8857dd54c60eaf98cc19c
coinhat <nhatcoifacebook@gmail.com>
2024-10-23 18:20:52 +0700

    feat: category controller

--- src/main/java/com/example/ecommerceweb/controllers/CategoryController.java
+++ src/main/java/com/example/ecommerceweb/controllers/CategoryController.java
@@ -1,31 +1,39 @@
 package com.example.ecommerceweb.controllers;
 
 import com.example.ecommerceweb.dtos.CategoryDTO;
+import com.example.ecommerceweb.services.CategoryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("categories")
+@RequiredArgsConstructor
 public class CategoryController {
-
+    private final CategoryService categoryService;
 
     @PostMapping("")
     public String createCategory(@ModelAttribute CategoryDTO categoryDTO) {
+        categoryService.createCategory(categoryDTO);
         return "redirect:/";
     }
 
     @GetMapping("")
-    public String getAllCategories() {
-        return "categories";
+    public String getAllCategories(Model model) {
+        model.addAttribute("categories", categoryService.getAllCategories());
+        return "index";
     }
 
     @PutMapping("/{id}")
     public String updateCategory(@PathVariable Long id, @ModelAttribute CategoryDTO categoryDTO) {
+        categoryService.updateCategory(id, categoryDTO);
         return "redirect:/";
     }
 
     @DeleteMapping("/{id}")
     public String deleteCategory(@PathVariable Long id) {
+        categoryService.deleteCategoryById(id);
         return "redirect:/";
     }
 }
