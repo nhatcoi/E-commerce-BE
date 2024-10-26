011dd9a9124ddf09a1c9083700925f92a66aa1b9
coinhat <nhatcoifacebook@gmail.com>
2024-10-26 17:01:56 +0700

    style: static variable

---
+++ src/main/java/com/example/ecommerceweb/utils/Static.java
@@ -0,0 +1,8 @@
+package com.example.ecommerceweb.utils;
+
+public class Static {
+    public static int PAGINATION_LIMIT = 8;
+    public static int LATEST_LIMIT = 5;
+    public static int TOP_RATING_LIMIT = 5;
+    public static int PAGE_SLIDE = 3;
+}
