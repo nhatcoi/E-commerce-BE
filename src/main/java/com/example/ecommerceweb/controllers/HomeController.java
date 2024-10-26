b13919aa161108a4e7163b0c2a936c0da11762ba
coinhat <nhatcoifacebook@gmail.com>
2024-10-26 16:59:21 +0700

    refactor: code improvement

--- src/main/java/com/example/ecommerceweb/controllers/HomeController.java
+++ src/main/java/com/example/ecommerceweb/controllers/HomeController.java
@@ -4,7 +4,6 @@ import com.example.ecommerceweb.entities.Category;
 import com.example.ecommerceweb.entities.Product;
 import com.example.ecommerceweb.services.CategoryService;
 import com.example.ecommerceweb.services.ProductService;
-import com.example.ecommerceweb.services.services_impl.ProductServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
@@ -12,11 +11,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import java.io.IOException;
 import java.util.List;
-
 import static com.example.ecommerceweb.utils.DivideList.divideList;
+import static com.example.ecommerceweb.utils.Static.*;
 
 @Controller
 @RequestMapping("")
@@ -24,15 +22,11 @@ import static com.example.ecommerceweb.utils.DivideList.divideList;
 public class HomeController {
     private final CategoryService categoryService;
     private final ProductService productService;
-    private final ProductServiceImpl productServiceImpl;
 
-    public static int PAGINATION_LIMIT = 8;
-    public static int LATEST_LIMIT = 5;
-    public static int TOP_RATING_LIMIT = 5;
-    public static int PAGE_SLIDE = 3;
 
     @GetMapping("")
     public String home(@RequestParam(defaultValue = "0") int page, Model model) throws IOException, InterruptedException {
+        List<Category> categories = categoryService.getAllCategories();
         Page<Product> productPage = productService.getProductsByPage(page, PAGINATION_LIMIT);
 
         List<List<Product>> latestProducts = divideList(
@@ -44,8 +38,7 @@ public class HomeController {
                 PAGE_SLIDE
         );
 
-
-        model.addAttribute("categories", categoryService.getAllCategories());
+        model.addAttribute("categories", categories);
         model.addAttribute("products", productPage.getContent());
         model.addAttribute("currentPage", page);
         model.addAttribute("totalPages", productPage.getTotalPages());
