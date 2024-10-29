823414a3d6acba6826a3392c8b557253daa812ab
coinhat <nhatcoifacebook@gmail.com>
2024-10-29 11:10:04 +0700

    feat: add mode via dto

--- src/main/java/com/example/ecommerceweb/EcommerceWebApplication.java
+++ src/main/java/com/example/ecommerceweb/EcommerceWebApplication.java
@@ -2,8 +2,12 @@ package com.example.ecommerceweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class EcommerceWebApplication {
 
     public static void main(String[] args) {
