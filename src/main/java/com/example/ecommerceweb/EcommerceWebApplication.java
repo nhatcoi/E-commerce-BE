cb7830ce2ed2d1b452cecfb0217964590a8624d4
coinhat <nhatcoifacebook@gmail.com>
2024-11-05 00:18:30 +0700

    feat: add dotenv

--- src/main/java/com/example/ecommerceweb/EcommerceWebApplication.java
+++ src/main/java/com/example/ecommerceweb/EcommerceWebApplication.java
@@ -1,5 +1,6 @@
 package com.example.ecommerceweb;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
@@ -11,6 +12,8 @@ import static org.springframework.data.web.config.EnableSpringDataWebSupport.Pag
 public class EcommerceWebApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(EcommerceWebApplication.class, args);
     }
 
