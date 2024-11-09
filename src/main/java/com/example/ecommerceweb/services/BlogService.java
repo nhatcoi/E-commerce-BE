922d3f59e42ad20761d9f239cc566cdd1021d7fa
coinhat <nhatcoifacebook@gmail.com>
2024-11-09 19:30:32 +0700

    feat: add blog service

---
+++ src/main/java/com/example/ecommerceweb/services/BlogService.java
@@ -0,0 +1,17 @@
+package com.example.ecommerceweb.services;
+
+import com.example.ecommerceweb.dtos.BlogDTO;
+import com.example.ecommerceweb.dtos.PaginatedResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface BlogService {
+    Page<BlogDTO> getAllBlogs(Pageable pageable);
+    List<BlogDTO> getRecentBlogs(int limit);
+    Page<BlogDTO> getBlogByCategory(Pageable pageable, Long categoryId);
+    PaginatedResponse<BlogDTO> createPaginatedResponse(Page<BlogDTO> blogPage);
+}
