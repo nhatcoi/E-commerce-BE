10d9784fe9eee7b0220a7b38dd262eb512b53217
coinhat <nhatcoifacebook@gmail.com>
2024-11-09 19:32:11 +0700

    fix: add find by page of product

--- src/main/java/com/example/ecommerceweb/repository/ProductRepository.java
+++ src/main/java/com/example/ecommerceweb/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.example.ecommerceweb.repository;
 
 import com.example.ecommerceweb.entities.Product;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,6 +11,9 @@ import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("SELECT p FROM Product p JOIN FETCH p.categoryId")
+    Page<Product> findAllProductsByPage(Pageable pageable);
 
     @Query("SELECT p FROM Product p JOIN FETCH p.categoryId ORDER BY p.createdAt DESC")
     List<Product> fetchLatestProducts(Pageable pageable);
