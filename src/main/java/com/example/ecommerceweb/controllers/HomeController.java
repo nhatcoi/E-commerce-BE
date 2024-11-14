f7c5cb7e64589922bb8b6a819dd98ff1d14f80e1
coinhat <nhatcoifacebook@gmail.com>
2024-11-14 13:43:06 +0700

    fix: escape redundant api

--- src/main/java/com/example/ecommerceweb/controllers/HomeController.java
+++ src/main/java/com/example/ecommerceweb/controllers/HomeController.java
@@ -2,25 +2,18 @@ package com.example.ecommerceweb.controllers;
 
 import com.example.ecommerceweb.dtos.CategoryDTO;
 import com.example.ecommerceweb.dtos.ProductDTO;
-import com.example.ecommerceweb.entities.Category;
-import com.example.ecommerceweb.entities.Product;
 import com.example.ecommerceweb.services.CategoryService;
 import com.example.ecommerceweb.services.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import static com.example.ecommerceweb.utils.DivideList.divideList;
@@ -59,18 +52,6 @@ public class HomeController {
         model.addAttribute("topRatedProducts", topRatedProducts);
         return "index";
     }
-
-    @GetMapping("/switch-page")
-    @ResponseBody
-    public Map<String, Object> getProducts(@RequestParam int page) {
-        Page<Product> productPage = productService.getProductsByPage(page, PAGINATION_LIMIT);
-        Map<String, Object> response = new HashMap<>();
-        response.put("products", productPage.getContent());
-        response.put("currentPage", page);
-        response.put("totalPages", productPage.getTotalPages());
-        return response;
-    }
-
 
 }
 
