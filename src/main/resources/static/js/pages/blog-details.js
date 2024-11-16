166c2df9280f1476b37265f74805c1532bfc96e0
coinhat <nhatcoifacebook@gmail.com>
2024-11-16 21:17:09 +0700

    feat: add ShoppingCartController

---
+++ src/main/resources/static/js/pages/blog-details.js
@@ -0,0 +1,44 @@
+'use restrict';
+
+(function ($) {
+    $(document).ready(function() {
+        // When clicking on the blog item link
+        function getQueryParam(param) {
+            const urlParams = new URLSearchParams(window.location.search);
+            return urlParams.get(param);
+        }
+
+        // Get the blog ID from the URL
+        const blogId = getQueryParam('id');
+
+        if (blogId) {
+            $.ajax({
+                url: `/blog-details/${blogId}`, // API endpoint to fetch blog details
+                type: 'GET',
+                success: function (data) {
+                    // Assuming `data` is a JSON object with all necessary fields
+                    $('#blog-title').text(data.title);
+                    $('#blog-author').text(`By ${data.userId}`);
+                    $('#blog-date').txxxxext(data.createAt);
+                    // $('#blog-comments').text(`${data.comments} Comments`);
+                    $('#blog-image').attr('src', data.thumbnail);
+                    $('#blog-content').text(data.content);
+                    // $('#author-image').attr('src', data.authorImage);
+                    // $('#author-name').text(data.authorName);
+                    // $('#author-role').text(data.authorRole);
+                    $('#blog-categories').text(data.categories.join(', '));
+                    $('#blog-tags').text(data.tags.join(', '));
+                },
+                error: function (xhr, status, error) {
+                    console.error('Failed to fetch blog details:', error);
+                    $('#blog-content').text('Error loading blog details.');
+                }
+            });
+        } else {
+            $('#blog-content').text('Blog ID not found.');
+        }
+    });
+
+
+
+} (jQuery));
