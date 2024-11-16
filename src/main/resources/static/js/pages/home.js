59740e8311a5626d93c981a96d3b954c359c64f7
coinhat <nhatcoifacebook@gmail.com>
2024-11-16 09:24:34 +0700

    fix(home): refactor code home layout

--- src/main/resources/static/js/pages/home.js
+++ src/main/resources/static/js/pages/home.js
@@ -1,112 +1,103 @@
-'use restrict';
+'use strict';
 
 (function ($) {
-    // home : switch page
+
+    // $(document).ready(function() {
+    //     const userId = localStorage.getItem('userId');
+    //     if (userId) {
+    //         console.log("User ID:", userId);
+    //     } else {
+    //         window.location.href = '/api/login-form';
+    //     }
+    // });
+
     $(document).ready(function () {
-        loadPageContent(0);
+        const PRODUCTS_URL = '/products';
+        const INITIAL_PAGE = 0;
+        const PAGE_SIZE = 8;
+        const PAGES_TO_SHOW = 2;
 
-        // Event listener for pagination buttons
-        $(".pagination").on("click", ".page-link", function (e) {
-            e.preventDefault();
-            let page = $(this).data("page");
-            let currentPage = parseInt($("#currentPage").val());
+        // loadCategories();
+        loadProducts(PRODUCTS_URL, INITIAL_PAGE);
 
-            if (page === "next") {
-                page = currentPage + 1;
-            } else if (page === "prev") {
-                page = currentPage - 1;
-            }
 
-            // Call the function to load content for the selected page
-            loadPageContent(page);
-        });
-    });
+        function loadProducts(url, page) {
+            $.ajax({
+                url: url,
+                method: 'GET',
+                data: { page: page, size: PAGE_SIZE },
+                success: function (data) {
+                    renderProducts(data.content);
+                    renderPagination(data.totalPages, data.currentPage, url);
+                },
+            });
+        }
 
-    function loadPageContent(page) {
-        $.ajax({
-            url: `/switch-page?page=${page}`, // call api switch page
-            type: "GET",
-            success: function (response) {
-                $("#product-list").html(""); // Clear current products
-                response.products.forEach(product => {
-                    $("#product-list").append(`
-                         <div class="col-lg-3 col-md-4 col-sm-6 mix">
-                            <div class="product__item">
-                                <div class="product__item__pic set-bg" style="background-image: url('${product.thumbnail}');">
-                                    <ul class="product__item__pic__hover">
-                                        <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                                        <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-                                        <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-                                    </ul>
-                                </div>
-                                <div class="product__item__text">
-                                    <h6><a href="/products/${product.id}">${product.name}</a></h6>
-                                    <h5>${product.price} $</h5>
-                                </div>
+        function renderProducts(products) {
+            let productContainer = $('#product-container');
+            productContainer.empty();
+
+            products.forEach(product => {
+                let productItem = `
+                    <div class="col-lg-3 col-md-4 col-sm-6 mix">
+                        <div class="product__item">
+                            <div class="product__item__pic set-bg" style="background-image: url('${product.thumbnail}');">
+                                <ul class="product__item__pic__hover">
+                                    <li><a href="#"><i class="fa fa-heart"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-retweet"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+                                </ul>
+                            </div>
+                            <div class="product__item__text">
+                                <h6><a href="/products/${product.id}">${product.name}</a></h6>
+                                <h5>${product.price} $</h5>
                             </div>
                         </div>
+                    </div>
+                `;
+                  productContainer.append(productItem);
+            });
+        }
+
+        function renderPagination(totalPages, currentPage, url) {
+            let pagination = $('.product__pagination.blog__pagination');
+            pagination.empty();
+
+            let startPage = Math.floor(currentPage / PAGES_TO_SHOW) * PAGES_TO_SHOW;
+            let endPage = Math.min(startPage + PAGES_TO_SHOW, totalPages);
+
+            let prevArrow = startPage > 0 ? `<a href="#" class="pagination-link fa fa-long-arrow-left" data-page="${startPage - 1}" data-url="${url}">&laquo;</a>` : '';
+            let nextArrow = endPage < totalPages ? `<a href="#" class="pagination-link fa fa-long-arrow-right" data-page="${endPage}" data-url="${url}">&raquo;</a>` : '';
+
+            pagination.append(prevArrow);
+
+            for (let i = startPage; i < endPage; i++) {
+                let activeClass = i === currentPage ? 'active' : '';
+                pagination.append(`
+                    <a href="#" class="pagination-link ${activeClass}" data-page="${i}" data-url="${url}">${i + 1}</a>
                 `);
-                });
-
-                // Update current page and total pages
-                $("#currentPage").val(page);
-                updatePagination(page, response.totalPages);
-            },
-            error: function () {
-                alert("Could not load products.");
             }
+
+            pagination.append(nextArrow);
+        }
+
+        // click page
+        $(document).on('click', '.pagination-link', function (e) {
+            e.preventDefault();
+            let page = $(this).data('page');
+            let url = $(this).data('url');
+            loadProducts(url, page);
         });
-    }
 
-    // Function to dynamically update pagination links
-    function updatePagination(currentPage, totalPages) {
-        let paginationHTML = `
-        <li id="prevPage" class="page-item ${currentPage === 0 ? 'disabled' : ''}">
-            <a class="page-link" href="#" data-page="prev" aria-label="Previous">
-                <span aria-hidden="true">&laquo;</span>
-            </a>
-        </li>
-    `;
 
-        for (let i = 0; i < totalPages; i++) {
-            paginationHTML += `
-            <li class="page-item ${i === currentPage ? 'active' : ''}">
-                <a class="page-link" href="#" data-page="${i}">${i + 1}</a>
-            </li>
-        `;
+        function handleError(message) {
+            return function () {
+                alert(message);
+            };
         }
-
-        paginationHTML += `
-        <li id="nextPage" class="page-item ${currentPage === totalPages - 1 ? 'disabled' : ''}">
-            <a class="page-link" href="#" data-page="next" aria-label="Next">
-                <span aria-hidden="true">&raquo;</span>
-            </a>
-        </li>
-    `;
-
-        $(".pagination").html(paginationHTML);
-    }
-
-    // Event listener for pagination links
-    $(".pagination").on("click", ".page-link", function (e) {
-        e.preventDefault();
-        let page = $(this).data("page");
-        let currentPage = parseInt($("#currentPage").val());
-
-        if (page === "next") {
-            page = currentPage + 1;
-        } else if (page === "prev") {
-            page = currentPage - 1;
-        } else {
-            page = parseInt(page);
-        }
-
-        loadPageContent(page);
     });
 
 
 
 
-
-
-}(jQuery));
+})(jQuery);
