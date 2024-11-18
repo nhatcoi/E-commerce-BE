4d49f44d5d91675887ce06fa5c13c21fec02442b
coinhat <nhatcoifacebook@gmail.com>
2024-11-19 03:16:59 +0700

    fix: enable Security

--- src/main/java/com/example/ecommerceweb/EcommerceWebApplication.java
+++ src/main/java/com/example/ecommerceweb/EcommerceWebApplication.java
@@ -3,11 +3,12 @@ package com.example.ecommerceweb;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class EcommerceWebApplication {
 
