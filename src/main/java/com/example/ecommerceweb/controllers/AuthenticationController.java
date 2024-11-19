e00620a8b61f3854036b4afebf960c983aebe44d
coinhat <nhatcoifacebook@gmail.com>
2024-11-19 15:24:34 +0700

    feat: add authenticate login

---
+++ src/main/java/com/example/ecommerceweb/controllers/AuthenticationController.java
@@ -0,0 +1,28 @@
+package com.example.ecommerceweb.controllers;
+
+import com.example.ecommerceweb.dtos.request.AuthenticationRequest;
+import com.example.ecommerceweb.dtos.response.ApiResponse;
+import com.example.ecommerceweb.dtos.response.AuthenticationResponse;
+import com.example.ecommerceweb.services.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/log-in")
+    public ResponseEntity<AuthenticationResponse> logIn(@RequestBody  AuthenticationRequest request) {
+        boolean result = authenticationService.authenticate(request).isAuthenticated();
+        return ResponseEntity.ok(AuthenticationResponse.builder().
+                isAuthenticated(result).
+                build());
+    }
+
+}
