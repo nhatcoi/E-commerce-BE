6800697c5bff877f7a2908fbfab93cb6644f634d
coinhat <nhatcoifacebook@gmail.com>
2024-11-20 18:48:09 +0700

    feat: add introspect token api

--- src/main/java/com/example/ecommerceweb/controllers/AuthenticationController.java
+++ src/main/java/com/example/ecommerceweb/controllers/AuthenticationController.java
@@ -1,13 +1,18 @@
 package com.example.ecommerceweb.controllers;
 
 import com.example.ecommerceweb.dtos.request.AuthenticationRequest;
+import com.example.ecommerceweb.dtos.request.IntrospectRequest;
 import com.example.ecommerceweb.dtos.response.ApiResponse;
 import com.example.ecommerceweb.dtos.response.AuthenticationResponse;
+import com.example.ecommerceweb.dtos.response.IntrospectResponse;
 import com.example.ecommerceweb.services.AuthenticationService;
+import com.nimbusds.jose.JOSEException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.text.ParseException;
 
 @Slf4j
 @RestController
@@ -25,6 +30,14 @@ public class AuthenticationController {
                         .isAuthenticated(result.isAuthenticated())
                         .build()
         );
+    }
+
+    @PostMapping("/introspect")
+    public ResponseEntity<?> introspect(@RequestBody IntrospectRequest request) throws ParseException, JOSEException {
+        var result = authenticationService.introspect(request);
+        return ResponseEntity.ok(IntrospectResponse.builder()
+                .valid(result.isValid())
+                .build());
     }
 
 }
