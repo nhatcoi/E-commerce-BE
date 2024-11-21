9878ed1e9c91fccdac24aaa0fbf42138d671ba62
coinhat <nhatcoifacebook@gmail.com>
2024-11-21 09:58:43 +0700

    feat: add blog details

--- src/main/java/com/example/ecommerceweb/controllers/BlogDetailController.java
+++ src/main/java/com/example/ecommerceweb/controllers/BlogDetailController.java
@@ -1,20 +1,31 @@
 package com.example.ecommerceweb.controllers;
 
+import com.example.ecommerceweb.dtos.BlogDTO;
+import com.example.ecommerceweb.services.BlogService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Controller
-@RequestMapping("/blog-details")
+@RequestMapping("blog-details")
 @RequiredArgsConstructor
 public class BlogDetailController {
+    private final BlogService blogService;
+
 
     @GetMapping("")
     public String blogDetail() {
         return "blog-details";
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity<?> getBlogDetail(@PathVariable Long id) {
+        BlogDTO blog = blogService.getBlogById(id);
+        return ResponseEntity.ok().body(blog);
     }
 
 }
