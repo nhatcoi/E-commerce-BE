da948dbe51ada79315f014f4d5ee22bd2b29b312
coinhat <nhatcoifacebook@gmail.com>
2024-11-21 09:49:17 +0700

    fix: drop security auto configuration

--- src/main/java/com/example/ecommerceweb/EcommerceWebApplication.java
+++ src/main/java/com/example/ecommerceweb/EcommerceWebApplication.java
@@ -3,12 +3,11 @@ package com.example.ecommerceweb;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class EcommerceWebApplication {
 
