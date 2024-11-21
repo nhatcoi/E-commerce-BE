5568070aaea3b6173bebe2b073e1c722c4d2d832
coinhat <nhatcoifacebook@gmail.com>
2024-11-21 09:59:40 +0700

    feat: add get user services

--- src/main/java/com/example/ecommerceweb/services/UserService.java
+++ src/main/java/com/example/ecommerceweb/services/UserService.java
@@ -4,9 +4,13 @@ import com.example.ecommerceweb.dtos.UserRequest;
 import com.example.ecommerceweb.dtos.UserResponse;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface UserService {
     UserResponse getUserByPhoneNumberAndPassword(String phoneNumber, String password);
-
     UserResponse createUser(UserRequest userRequest);
+    List<UserResponse> getUsers();
+    UserResponse getUserById(Long userId);
+
 }
