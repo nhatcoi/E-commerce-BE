dbb75df6983ccbafdafd837b0653629c0ebd4384
coinhat <nhatcoifacebook@gmail.com>
2024-11-22 18:01:19 +0700

    Refactor: RoleEnum

--- src/main/java/com/example/ecommerceweb/enums/RoleEnum.java
+++ src/main/java/com/example/ecommerceweb/enums/RoleEnum.java
@@ -1,13 +1,13 @@
 package com.example.ecommerceweb.enums;
 
-public enum Role {
+public enum RoleEnum {
     ADMIN(1),
     STAFF(2),
     USER(3);
 
     private final int value;
 
-    Role(int value) {
+    RoleEnum(int value) {
         this.value = value;
     }
 
