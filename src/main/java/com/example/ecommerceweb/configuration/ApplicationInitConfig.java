210c5bf9c34c0f55db8b01bc93cf9470517d5547
coinhat <nhatcoifacebook@gmail.com>
2024-11-23 23:07:14 +0700

    chore: env admin

--- src/main/java/com/example/ecommerceweb/configuration/ApplicationInitConfig.java
+++ src/main/java/com/example/ecommerceweb/configuration/ApplicationInitConfig.java
@@ -5,6 +5,7 @@ import com.example.ecommerceweb.enums.RoleEnum;
 import com.example.ecommerceweb.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,16 +18,22 @@ public class ApplicationInitConfig {
     @Autowired
     PasswordEncoder passwordEncoder;
 
+    @Value("${admin.username}")
+    private String adminUsername;
+
+    @Value("${admin.password}")
+    private String adminPassword;
+
     @Bean
     ApplicationRunner applicationRunner(UserRepository userRepository) {
         return args -> {
-            if (userRepository.findByPhoneNumber("123456").isEmpty()) {
+            if (userRepository.findByPhoneNumber(adminUsername).isEmpty()) {
                 var admin = new com.example.ecommerceweb.entity.User();
-                admin.setPhoneNumber("123456");
-                admin.setPassword(passwordEncoder.encode("admin"));
+                admin.setPhoneNumber(adminUsername);
+                admin.setPassword(passwordEncoder.encode(adminPassword));
                 admin.setRole(Role.builder().id(RoleEnum.ADMIN.getValue()).build());
                 userRepository.save(admin);
-                log.warn("Admin user created with phone number 123456 and password admin");
+                log.warn("Admin user created");
             }
         };
     }
