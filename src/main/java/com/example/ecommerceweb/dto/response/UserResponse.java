0b516911c740c0142f7b44efe13080235c7317c9
coinhat <nhatcoifacebook@gmail.com>
2024-11-25 08:57:30 +0700

    feat: add username, update roles in UserResponse

--- src/main/java/com/example/ecommerceweb/dto/response/UserResponse.java
+++ src/main/java/com/example/ecommerceweb/dto/response/UserResponse.java
@@ -3,6 +3,7 @@ package com.example.ecommerceweb.dto.response;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Data
 @Getter
@@ -13,8 +14,9 @@ import java.time.LocalDateTime;
 public class UserResponse {
     private Long id;
     private String fullName;
+    private String username;
     private String phoneNumber;
     private String address;
     private LocalDateTime DateOfBirth;
-    private Integer roleId;
+    private Set<String> roleNames;
 }
