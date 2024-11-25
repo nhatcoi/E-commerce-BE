5f5b6f8b1b1a05e8ddfe7d4c73e25522e9f9aacf
coinhat <nhatcoifacebook@gmail.com>
2024-11-25 14:09:04 +0700

    refactor: modify code by mapstruct

--- src/main/java/com/example/ecommerceweb/service/services_impl/UserServiceImpl.java
+++ src/main/java/com/example/ecommerceweb/service/services_impl/UserServiceImpl.java
@@ -7,10 +7,14 @@ import com.example.ecommerceweb.entity.User;
 import com.example.ecommerceweb.enums.RoleEnum;
 import com.example.ecommerceweb.exception.ErrorCode;
 import com.example.ecommerceweb.exception.ResourceNotFoundException;
+import com.example.ecommerceweb.mapper.UserMapper;
 import com.example.ecommerceweb.repository.UserRepository;
 import com.example.ecommerceweb.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -20,22 +24,18 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-    private final ModelMapper modelMapper;
+    private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserResponse getUserByPhoneNumberAndPassword(String phoneNumber, String password) {
         User user = userRepository.findByPhoneNumberAndPassword(phoneNumber, password);
         if (user != null) {
-            return UserResponse.builder()
-                    .id(user.getId())
-                    .fullName(user.getFullName())
-                    .phoneNumber(user.getPhoneNumber())
-                    .address(user.getAddress())
-                    .build();
+            return userMapper.toUserResponse(user);
         }
         return null;
     }
@@ -51,64 +51,39 @@ public class UserServiceImpl implements UserService {
         Set<Role> roles = Set.of(Role.builder()
                 .id(RoleEnum.USER.getValue())
                 .name(RoleEnum.USER.name()).build());
-        User user = User.builder()
-                .username(userRequest.getUsername())
-                .password(userRequest.getPassword())
-                .phoneNumber(userRequest.getPhoneNumber())
-                .fullName(userRequest.getFullName())
-                .address(userRequest.getAddress())
-                .dateOfBirth(userRequest.getDateOfBirth())
-                .isActive(true)
-                .roles(roles)
-                .build();
+        User user = userMapper.toUser(userRequest);
+        user.setRoles(roles);
         user.setPassword(passwordEncoder.encode(userRequest.getPassword()));
         userRepository.save(user);
 
-        return UserResponse.builder()
-                .id(user.getId())
-                .fullName(user.getFullName())
-                .username(user.getUsername())
-                .phoneNumber(user.getPhoneNumber())
-                .address(user.getAddress())
-                .dateOfBirth(user.getDateOfBirth())
-                .roleNames(user.getRoles().stream()
-                        .map(Role::getName)
-                        .collect(Collectors.toSet()))
-                .build();
+        return userMapper.toUserResponse(user);
     }
 
+    public UserResponse getMyInfo() {
+        var context = SecurityContextHolder.getContext();
+        context.getAuthentication().getName();
+
+        User user = userRepository.findByUsername(context.getAuthentication().getName())
+                .orElseThrow(() -> new ResourceNotFoundException(ErrorCode.USER_NOT_EXISTED.getMessage()));
+        return userMapper.toUserResponse(user);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
     @Override
     public List<UserResponse> getUsers() {
+        log.info("Get all users");
         List<User> users = userRepository.findAll();
         return users.stream()
-                .map(user -> UserResponse.builder()
-                        .id(user.getId())
-                        .fullName(user.getFullName())
-                        .username(user.getUsername())
-                        .phoneNumber(user.getPhoneNumber())
-                        .address(user.getAddress())
-                        .dateOfBirth(user.getDateOfBirth())
-                        .roleNames(user.getRoles().stream()
-                                .map(Role::getName)
-                                .collect(Collectors.toSet()))
-                        .build())
-                .toList();
+                .map(userMapper::toUserResponse)
+                .collect(Collectors.toList());
     }
 
+    @PostAuthorize("returnObject.username == authentication.name")
     @Override
     public UserResponse getUserById(Long userId) {
+        log.info("Get user by id: {}", userId);
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new ResourceNotFoundException(ErrorCode.USER_NOT_EXISTED.getMessage()));
-        return UserResponse.builder()
-                .id(user.getId())
-                .fullName(user.getFullName())
-                .username(user.getUsername())
-                .phoneNumber(user.getPhoneNumber())
-                .address(user.getAddress())
-                .dateOfBirth(user.getDateOfBirth())
-                .roleNames(user.getRoles().stream()
-                        .map(Role::getName)
-                        .collect(Collectors.toSet()))
-                .build();
+        return userMapper.toUserResponse(user);
     }
 }
