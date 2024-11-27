06f2c40af5e6a2fa9e4fccddd0f8aec2c7d4fda8
coinhat <nhatcoifacebook@gmail.com>
2024-11-27 18:12:39 +0700

    fix: switch to RestController and Authority

--- src/main/java/com/example/ecommerceweb/controller/ShoppingCartController.java
+++ src/main/java/com/example/ecommerceweb/controller/ShoppingCartController.java
@@ -1,39 +1,39 @@
 package com.example.ecommerceweb.controller;
 
-import com.example.ecommerceweb.entity.CartItem;
+import com.example.ecommerceweb.configuration.Translator;
+import com.example.ecommerceweb.dto.response.ResponseData;
+import com.example.ecommerceweb.entity.Cart;
 import com.example.ecommerceweb.service.CartService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Slf4j
-@Controller
+@RestController
 @RequestMapping("${api.prefix}/shopping-cart")
 @RequiredArgsConstructor
 public class ShoppingCartController {
 
     private final CartService cartService;
 
-    @GetMapping("")
-    public String shoppingCart() {
-        return "shopping-cart";
-    }
+    private final Translator translator;
 
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/{cartId}")
-    @ResponseBody
     public ResponseEntity<?> getItemsCart(@PathVariable Long cartId)
     {
-        List<CartItem> cartItems = cartService.getCartItems(cartId);
+        List<Cart> cartItems = cartService.getCartItems(cartId);
         return ResponseEntity.ok().body(cartItems);
     }
 
-    @GetMapping("/count/{cartId}")
-    @ResponseBody
-    public ResponseEntity<?> getCountInCart(@PathVariable Long cartId) {
-        return ResponseEntity.ok().body(cartService.getCountInCart(cartId));
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/count")
+    public ResponseData<?> getCountInCart() {
+        return new ResponseData<>(HttpStatus.OK.value(),  translator.toLocated("response.success"), cartService.getCountInCart());
     }
 }
