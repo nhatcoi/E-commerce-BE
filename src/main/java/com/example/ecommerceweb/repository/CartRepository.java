21e827c95072c02795cdae5eeedfdcc4346122a2
coinhat <nhatcoifacebook@gmail.com>
2024-11-27 18:14:04 +0700

    feat: add findByUserId and countByUsername jpa

--- src/main/java/com/example/ecommerceweb/repository/CartRepository.java
+++ src/main/java/com/example/ecommerceweb/repository/CartRepository.java
@@ -2,9 +2,15 @@ package com.example.ecommerceweb.repository;
 
 import com.example.ecommerceweb.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
+    List<Cart> findByUserId(Long userId);
 
+    @Query("SELECT COUNT(c) FROM Cart c WHERE c.user.username = ?1")
+    Integer countByUsername(String username);
 }
