8f44749fd4202d86c15e88bd76efa7e5a0f4c6a4
coinhat <nhatcoifacebook@gmail.com>
2024-11-29 23:34:27 +0700

    switch to rest

--- src/main/java/com/example/ecommerceweb/controller/ProductController.java
+++ src/main/java/com/example/ecommerceweb/controller/ProductController.java
@@ -1,7 +1,9 @@
 package com.example.ecommerceweb.controller;
 
+import com.example.ecommerceweb.configuration.Translator;
 import com.example.ecommerceweb.dto.PaginatedResponse;
 import com.example.ecommerceweb.dto.ProductDTO;
+import com.example.ecommerceweb.dto.response.ResponseData;
 import com.example.ecommerceweb.entity.Product;
 import com.example.ecommerceweb.service.ProductService;
 import com.example.ecommerceweb.service.services_impl.ProductImageService;
@@ -10,22 +12,27 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
+import java.util.List;
+
+import static com.example.ecommerceweb.util.DivideList.divideList;
+import static com.example.ecommerceweb.util.Static.*;
 
 @RequestMapping("${api.prefix}/products")
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class ProductController {
 
     private final ProductService productService;
     private final ProductImageService productImageService;
     private final ProductRatingService productRatingService;
+    private final Translator translator;
 
     @GetMapping("/{id}")
     public String getProductById(@PathVariable Long id, Model model) throws IOException {
@@ -57,17 +64,36 @@ public class ProductController {
     }
 
 
-    @GetMapping("/categories/{id}")
-    public ResponseEntity<PaginatedResponse<ProductDTO>> getProductsByCategory(
-            @PathVariable Long id,
+    @GetMapping("/category/{cateId}")
+    public ResponseData<?> getProductsByCategory(
+            @PathVariable Long cateId,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "4") int size) {
 
         Pageable pageable = PageRequest.of(page, size);
-        Page<ProductDTO> productsPage = productService.getProductsByCategory(pageable, id);
-        PaginatedResponse<ProductDTO> response = productService.createPaginatedResponse(productsPage);
+        Page<ProductDTO> productsPage = productService.getProductsByCategory(pageable, cateId);
+        List<ProductDTO> products = productsPage.getContent();
 
-        return ResponseEntity.ok(response);
+        return new ResponseData<>(HttpStatus.OK.value(), translator.toLocated("response.success"), products);
+    }
+
+
+    @GetMapping("/latest")
+    public ResponseData<?> getLatestProducts() {
+        List<List<ProductDTO>> latestProducts = divideList(
+                productService.getLatestProducts(LATEST_LIMIT),
+                PAGE_SLIDE
+        );
+        return new ResponseData<>(HttpStatus.OK.value(), translator.toLocated("response.success"), latestProducts);
+    }
+
+    @GetMapping("/top-rated")
+    public ResponseData<?> getTopRatedProducts() {
+        List<List<ProductDTO>> topRatedProducts = divideList(
+                productService.getTopRatedProducts(TOP_RATING_LIMIT),
+                PAGE_SLIDE
+        );
+        return new ResponseData<>(HttpStatus.OK.value(), translator.toLocated("response.success"), topRatedProducts);
     }
 
 }
