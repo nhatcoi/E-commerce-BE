6e41b978adeb50b249281be22717a86894496955
coinhat <nhatcoifacebook@gmail.com>
2024-12-05 21:29:03 +0700

    field cart fix

--- src/main/java/com/example/ecommerceweb/entity/Cart.java
+++ src/main/java/com/example/ecommerceweb/entity/Cart.java
@@ -11,17 +11,17 @@ import lombok.*;
 @Entity
 @Builder
 public class Cart extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
 
     @ManyToOne
-    @JoinColumn(name = "product_id", nullable = false)
+    @JoinColumn(name = "product_id", referencedColumnName = "id")
     private Product product;
 
     @Column(name = "quantity", nullable = false)
