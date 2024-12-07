f10046690db87ef7da091d508d30263fba4ce3f4
coinhat <nhatcoifacebook@gmail.com>
2024-12-07 12:58:09 +0700

    feat: crud cart

--- src/main/java/com/example/ecommerceweb/service/services_impl/CartServiceImpl.java
+++ src/main/java/com/example/ecommerceweb/service/services_impl/CartServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.ecommerceweb.service.services_impl;
 
 import com.example.ecommerceweb.entity.Cart;
+import com.example.ecommerceweb.exception.ErrorCode;
+import com.example.ecommerceweb.exception.ResourceException;
 import com.example.ecommerceweb.repository.CartRepository;
 import com.example.ecommerceweb.service.CartService;
 import lombok.RequiredArgsConstructor;
@@ -19,31 +21,40 @@ public class CartServiceImpl implements CartService {
 
 
     @Override
-    public List<Cart> getCartItems(Long userId) {
-        var context = SecurityContextHolder.getContext();
-        context.getAuthentication().getName();
-
-
-
-        return cartRepository.findByUserId(userId);
+    public List<Cart> getCartItems() {
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        return cartRepository.findByUserName(username);
     }
 
     @Override
     public Integer getCountInCart() {
-        var context = SecurityContextHolder.getContext();
-        var authentication = context.getAuthentication();
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (authentication == null || !authentication.isAuthenticated()) {
-            log.warn("User is not authenticated");
             throw new IllegalStateException("User is not authenticated");
         }
-
         String username = authentication.getName();
-        log.info("username: {}", username);
-
         Integer count = cartRepository.countByUsername(username);
-        log.info("count: {}", count);
-
         return count != null ? count : 0;
+    }
+
+    @Override
+    public void removeItem(Long id) {
+        cartRepository.findById(id).ifPresentOrElse(
+                cart -> cartRepository.deleteById(id),
+                () -> { throw new ResourceException(ErrorCode.CART_NOT_FOUND); }
+        );
+    }
+
+    @Override
+    public void updateCartItem(Long id, Integer quantity) {
+        cartRepository.findById(id).ifPresentOrElse(
+                cart -> {
+                    cart.setQuantity(quantity);
+                    cartRepository.save(cart);
+                },
+                () -> { throw new ResourceException(ErrorCode.CART_NOT_FOUND); }
+        );
     }
 }
