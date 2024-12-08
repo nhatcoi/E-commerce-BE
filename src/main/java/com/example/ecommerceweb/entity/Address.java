ba7c8c08720014cf1bb7589a7f0436ba3e304145
coinhat <nhatcoifacebook@gmail.com>
2024-12-08 23:57:01 +0700

    feat: add address entity

---
+++ src/main/java/com/example/ecommerceweb/entity/Address.java
@@ -0,0 +1,42 @@
+package com.example.ecommerceweb.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "user_address")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY) // Một địa chỉ chỉ thuộc về một User
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonBackReference
+    private User user;
+
+    @Column(name = "address_line", nullable = false)
+    private String addressLine;
+
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    @Column(name = "district")
+    private String district;
+
+    @Column(name = "postcode")
+    private String postcode;
+
+    @Column(name = "country")
+    private String country;
+}
