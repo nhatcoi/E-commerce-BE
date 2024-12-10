82c8aad4bb6ac67ba4c4b9c25171861f7ad6a2f2
coinhat <nhatcoifacebook@gmail.com>
2024-12-10 16:04:32 +0700

    feat: add to cart in shop-details

--- src/main/resources/static/js/pages/shop-details.js
+++ src/main/resources/static/js/pages/shop-details.js
@@ -1,5 +1,18 @@
 'use restrict';
 
 (function ($) {
+    const { API, Utils } = App;
+
+    function init() {
+        $("#filterButton").trigger("click");
+        $(document).on('click', '.add-to-cart', handleAddToCart);
+    }
+    $(document).ready(init);
+
+    function handleAddToCart(event) {
+        event.preventDefault();
+        const productId = $(this).data('id');
+        Utils.addToCartHandler(productId);
+    }
 
 } (jQuery));
