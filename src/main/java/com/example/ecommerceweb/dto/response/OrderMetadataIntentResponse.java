0889635f5d80fd443b4b56dde6def13c38bd9e41
coinhat <nhatcoifacebook@gmail.com>
2024-12-22 17:21:29 +0700

    feat: add OrderMetadataIntentResponse

---
+++ src/main/java/com/example/ecommerceweb/dto/response/OrderMetadataIntentResponse.java
@@ -0,0 +1,17 @@
+package com.example.ecommerceweb.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderMetadataIntentResponse {
+    private Long orderId;
+    private String fullName;
+    private String email;
+    private String phoneNumber;
+}
