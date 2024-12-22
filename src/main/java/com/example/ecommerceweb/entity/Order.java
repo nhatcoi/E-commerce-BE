d6b2c19ba91aca17353d31f0baa51a0d6a85cf3d
coinhat <nhatcoifacebook@gmail.com>
2024-12-22 17:20:31 +0700

    feat: add Order entity

---
+++ src/main/java/com/example/ecommerceweb/entity/Order.java
@@ -0,0 +1,72 @@
+package com.example.ecommerceweb.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "note")
+    private String note;
+
+    @Column(name = "order_date")
+    private LocalDateTime orderDate;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "total_price")
+    private Float totalPrice;
+
+    @Column(name = "shipping_method")
+    private String shippingMethod;
+
+    @Column(name = "shipping_address")
+    private String shippingAddress;
+
+    @Column(name = "shipping_date")
+    private LocalDateTime shippingDate;
+
+    @Column(name = "tracking_number")
+    private String trackingNumber;
+
+    @Column(name = "payment_method")
+    private String paymentMethod;
+
+    @Column(name = "payment_date")
+    private LocalDateTime paymentDate;
+
+    @Column(name = "active")
+    private Boolean active;
+}
