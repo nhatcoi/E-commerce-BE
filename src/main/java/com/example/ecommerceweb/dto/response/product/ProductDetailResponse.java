b4eca70222d62b7b75ab40c9c188c80a9103e367
nhatcoi <nhatcoifacebook@gmail.com>
2025-03-26 00:02:13 +0700

    feat(DTO): add ProductDetailResponse

---
+++ src/main/java/com/example/ecommerceweb/dto/response/product/ProductDetailResponse.java
@@ -0,0 +1,32 @@
+package com.example.ecommerceweb.dto.response.product;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDetailResponse {
+
+    private Long id;
+    private String name;
+    private Float price;
+    private String thumbnail;
+    private String description;
+
+    @JsonProperty("quantity_in_stock")
+    private Integer quantityInStock;
+    @JsonProperty("category_id")
+    private Long categoryId;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private Float avgRating;
+    private List<String> productImages;
+    private List<ProductSpecificationDTO> specifications;
+    private List<ProductAttributeDTO> attributes;
+}
