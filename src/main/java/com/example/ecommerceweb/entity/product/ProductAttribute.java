b53f8fdce717d564ae71a1f63ab189fe207c6492
nhatcoi <nhatcoifacebook@gmail.com>
2025-03-26 00:01:54 +0700

    feat(entity): add ProductAttribute

---
+++ src/main/java/com/example/ecommerceweb/entity/product/ProductAttribute.java
@@ -0,0 +1,36 @@
+package com.example.ecommerceweb.entity.product;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "product_attribute")
+public class ProductAttribute {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "attribute_value_id")
+    private AttributeValue attributeValue;
+
+    @Column(name = "price")
+    private Float price;
+
+    @Column(name = "stock_quantity")
+    private Integer stockQuantity;
+}
