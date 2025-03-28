b910a01691c8a756317cd12372c4c79e35fcbd76
nhatcoi <nhatcoifacebook@gmail.com>
2025-03-28 12:14:18 +0700

    feat(product): add id field to ProductAttributeDTO for better identification

--- src/main/java/com/example/ecommerceweb/dto/response/product/ProductAttributeDTO.java
+++ src/main/java/com/example/ecommerceweb/dto/response/product/ProductAttributeDTO.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class ProductAttributeDTO {
+    private Long id;
     private String attributeName;
     private String attributeValue;
     private Float price;
