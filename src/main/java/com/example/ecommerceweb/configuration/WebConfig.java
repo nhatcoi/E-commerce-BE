48ceb6317efbcf8ae8c7b555d231cb36af4bf87b
nhatcoi <nhatcoifacebook@gmail.com>
2025-03-28 12:15:32 +0700

    fix(cors): enable credentials support in CORS configuration

--- src/main/java/com/example/ecommerceweb/configuration/WebConfig.java
+++ src/main/java/com/example/ecommerceweb/configuration/WebConfig.java
@@ -10,7 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
+                .allowCredentials(true) // allow cookies
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+        ;
     }
 }
