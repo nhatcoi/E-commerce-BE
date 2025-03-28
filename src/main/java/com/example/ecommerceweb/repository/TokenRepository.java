bb157e001c9e724288ee6f6cae0a1d8608efd8ef
nhatcoi <nhatcoifacebook@gmail.com>
2025-03-28 12:16:05 +0700

    feat(token): add method to find Token by its value

--- src/main/java/com/example/ecommerceweb/repository/TokenRepository.java
+++ src/main/java/com/example/ecommerceweb/repository/TokenRepository.java
@@ -3,5 +3,9 @@ package com.example.ecommerceweb.repository;
 import com.example.ecommerceweb.entity.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TokenRepository extends JpaRepository<Token, Long> {
+
+    Optional<Token> findByToken(String token);
 }
