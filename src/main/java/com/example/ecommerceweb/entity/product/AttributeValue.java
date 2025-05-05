a3adbc999d308d86df750dd5c00d5a5b38c08daf
nhatcoi <nhatcoifacebook@gmail.com>
2025-05-05 14:51:41 +0700

    fix(attribute): change id type from Long to Integer

--- src/main/java/com/example/ecommerceweb/entity/product/AttributeValue.java
+++ src/main/java/com/example/ecommerceweb/entity/product/AttributeValue.java
@@ -16,7 +16,7 @@ public class AttributeValue {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     @ManyToOne
     @JoinColumn(name = "attribute_id")
